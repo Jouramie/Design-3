c8e79b44730f5f3af3d3f70c3c371542e85e5f8a
Jérémie Bolduc <jeremie.bolduc.1@ulaval.ca>
2018-02-06 11:56:18 -0500

    add remote before branch

--- robotbootscript.bash
+++ robotbootscript.bash
@@ -2,9 +2,9 @@
 
 # Update robot
 git remote add robot design3@10.42.0.1:~/robot
-git checkout robot
-git push robot
-git checkout master
+git checkout origin/robot
+git push robot/robot
+git checkout origin/master
 
 
 # TODO start python script on the robot
