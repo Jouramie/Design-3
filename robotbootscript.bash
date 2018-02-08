d5081e249bb5c5dc4986efa88b8fbb9fa6edecc7
Jérémie Bolduc <jeremie.bolduc.1@ulaval.ca>
2018-02-08 08:38:17 -0500

    update script

--- robotbootscript.bash
+++ robotbootscript.bash
@@ -2,9 +2,9 @@
 
 # Update robot
 git remote add robot design3@10.42.0.1:~/robot
-git checkout origin/robot
-git push robot/robot
-git checkout origin/master
+git checkout robot
+git push -f robot/robot
+git checkout master
 
 
 # TODO start python script on the robot
