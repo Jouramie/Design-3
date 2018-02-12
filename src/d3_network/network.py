a7f0735f63d51a6acbf551264758bf7dc1070afe
Jérémie Bolduc <jeremie.bolduc.1@ulaval.ca>
2018-02-12 11:12:00 -0500

    create simple tcp client/server

--- src/d3_network/network.py
+++ src/d3_network/network.py
@@ -1,2 +1,31 @@
+import socket
+
+host_location = socket.gethostname()
+port = 15555
+encoding = 'ascii'
+
+
+def host_network():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((host_location, port))
+
+    while True:
+        server.listen(5)
+        client, address = server.accept()
+        print("{} connected".format(address))
+
+        msg = "ThAnKs YoU fOr CoNnEcTiNg !!!!1\n"
+        client.send(msg.encode(encoding))
+        client.close()
+
+
 def pair_with_host():
-    print("Hello host!")
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((host_location, port))
+    msg = client.recv(1024)
+
+    client.close()
+    print(msg.decode(encoding))
+
+
+
