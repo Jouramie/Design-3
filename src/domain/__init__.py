be4094448d32164023fea8c1333e9fac9e38e506
Marek <marek.tremblay.1@ulaval.ca>
2018-02-13 12:21:39 -0500

    Cleaning of __int__

--- src/domain/__init__.py
+++
@@ -1,9 +0,0 @@
-from colour import Colour
-from country import Country
-from cube import Cube
-from stylizedFlag import StylizedFlag
-from point import Point
-from pathCalculator.vertex import Vertex
-from pathCalculator.graph import Graph
-from pathCalculator.pathCalculator import PathCalculator
-
