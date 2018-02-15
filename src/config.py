3ab1b0f393656ddd2036e511d9ffc6c41236e5fe
genou <genevieve.gilbert.6@ulaval.ca>
2018-02-15 12:13:05 -0500

    world_cam

--- src/config.py
+++ src/config.py
@@ -1,3 +1,5 @@
 WORLD_CAM_LOG_DIR = '../../log/'
 WORLD_CAM_LOG_FILE = WORLD_CAM_LOG_DIR + 'world_cam.log'
+WORLD_CAM_CALIBRATION_DIR = '../../calibration/'
+WORLD_CAM_CALIBRATION_OUTPUT = WORLD_CAM_CALIBRATION_DIR + 'world_cam_calibration_output.npz'
 FIG_DIRECTORY = "../../fig/"
--- src/vision/worldCamera.py
+++ src/vision/worldCamera.py
@@ -4,13 +4,15 @@ import platform
 import time
 
 import cv2
-
-from src import config
-from ..vision.camera import Camera
+import matplotlib.pyplot as plt
+import numpy as np
+from src.config import WORLD_CAM_CALIBRATION_OUTPUT, WORLD_CAM_LOG_DIR, WORLD_CAM_LOG_FILE, FIG_DIRECTORY, \
+    WORLD_CAM_CALIBRATION_DIR
+from src.vision.camera import Camera
 
 
 class WorldCamera(Camera):
-    def __init__(self):
+    def __init__(self, log_level=logging.INFO):
         camera_id = None
         system = platform.system()
         if system == 'Linux':
@@ -21,10 +23,15 @@ class WorldCamera(Camera):
             camera_id = 1
         self.id = camera_id
         self.capture_object = None
-        if not os.path.exists(config.WORLD_CAM_LOG_DIR):
-            os.makedirs(config.WORLD_CAM_LOG_DIR)
+        self.camera_matrix = None
+        self.distortion_coefs = None
+
+        if not os.path.exists(WORLD_CAM_LOG_DIR):
+            os.makedirs(WORLD_CAM_LOG_DIR)
+
         # TODO set logging level with args at startup
-        logging.basicConfig(level=logging.INFO, filename=config.WORLD_CAM_LOG_FILE, format='%(asctime)s %(message)s')
+        logging.basicConfig(level=log_level, filename=WORLD_CAM_LOG_FILE, format='%(asctime)s %(message)s')
+
         self._initialize()
 
     def _initialize(self):
@@ -40,16 +47,56 @@ class WorldCamera(Camera):
         is_frame_returned, img = self.capture_object.read()
         if is_frame_returned:
             logging.info('Picture taken')
-            directory = config.FIG_DIRECTORY + time.strftime("%Y-%m-%d")
+            directory = FIG_DIRECTORY + time.strftime("%Y-%m-%d")
+
             if not os.path.exists(directory):
                 os.makedirs(directory)
+
             cv2.imwrite(directory + time.strftime("/%Hh%M.jpg"), img)
             return img
         else:
             logging.info('No frame was returned while taking a picture')
             return 0
 
+    def calibrate(self, img_path, inside_corners_in_x, inside_corners_in_y):
+        logging.info('Get calibration parameters')
+        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+        # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+        objp = np.zeros((inside_corners_in_x * inside_corners_in_y, 3), np.float32)
+        objp[:, :2] = np.mgrid[0:inside_corners_in_y, 0:inside_corners_in_x].T.reshape(-1, 2)
+        # Arrays to store object points and image points from all the images.
+        obj_points = []  # 3d point in real world space
+        img_points = []  # 2d points in image plane.
+
+        logging.info('Processing image {}'.format(img_path))
+        img = cv2.imread(img_path)
+
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        is_chessboard_found, corners = cv2.findChessboardCorners(gray, (inside_corners_in_x, inside_corners_in_y), None)
+
+        if is_chessboard_found:
+            logging.info('Chessboard found')
+            img_points.append(corners)
+            obj_points.append(objp)
+
+            corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+            cv2.drawChessboardCorners(img, (inside_corners_in_x, inside_corners_in_y), corners2, is_chessboard_found)
+
+            ret, camera_matrix, distortion_coefs, rotation_vectors, translation_vectors = \
+                cv2.calibrateCamera(obj_points, img_points, gray.shape[::-1], None, None)
+
+            if not os.path.exists(WORLD_CAM_CALIBRATION_DIR):
+                os.makedirs(WORLD_CAM_CALIBRATION_DIR)
+
+            np.savez(WORLD_CAM_CALIBRATION_OUTPUT, ret=ret, camera_matrix=camera_matrix,
+                     distortion_coefs=distortion_coefs, rotation_vectors=rotation_vectors,
+                     translation_vectors=translation_vectors)
+        else:
+            logging.info('Chessboard not found')
 
 if __name__ == '__main__':
     world_camera = WorldCamera()
     world_camera.take_picture()
+    world_camera.calibrate('../../fig/2018-02-12/09h58.jpg', 7, 7)
+
