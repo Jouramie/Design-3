22338cceb5831085072a30edec506401f6e978c6
Jérémie Bolduc <jeremie.bolduc.1@ulaval.ca>
2018-02-19 12:50:47 -0500

    reworking tests

--- tests/robot_software/test_robot_controller.py
+++ tests/robot_software/test_robot_controller.py
@@ -7,24 +7,20 @@ from src.robot_software import robot_controller
 
 def test_when_start_controller_then_get_host_ip():
     network_scanner = MagicMock()
-    network = MagicMock()
-    ctrl = robot_controller.RobotController(network_scanner, network)
+    ctrl = robot_controller.RobotController(network_scanner, MagicMock())
 
     ctrl.start()
 
-    assert network_scanner.method_calls == [call.get_host_ip()]
+    network_scanner.get_host_ip.assert_called_once()
 
 
 def test_given_host_ip_when_start_controller_then_pair_with_host():
     host_ip = '10.42.0.78'
     network_scanner = Mock()
-    get_host_ip = Mock()
-    get_host_ip.return_value = host_ip
-    network_scanner.attach_mock(get_host_ip, 'get_host_ip')
-
+    network_scanner.attach_mock(Mock(return_value=host_ip), 'get_host_ip')
     network = MagicMock()
     ctrl = robot_controller.RobotController(network_scanner, network)
 
     ctrl.start()
 
-    assert network.method_calls == [call.pair_with_host(host_ip)]
+    network.pair_with_host.assert_called_once_with(host_ip)
