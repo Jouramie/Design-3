25ab7f0d0db02a15d9e9a3acfbe4ea5438ab4347
genou <genevieve.gilbert.6@ulaval.ca>
2018-03-10 14:31:43 -0500

    --amend

--- src/robot_hardware/channel/writer.py
+++ src/robot_hardware/channel/writer.py
@@ -1,5 +1,9 @@
 import serial
 
+from src.robot_hardware.channel.listener import create_channel_listener
+from src.robot_hardware.command import Command
+from src.robot_hardware.command_builder import CommandBuilder
+
 
 class ChannelWriter():
 
@@ -15,7 +19,7 @@ def wrapper(message):
 def create_channel_writer():
     ser = serial.Serial()
     ser.port = "/dev/ttyUSB0"
-    ser.baudrate = 1200
+    ser.baudrate = 115200
     ser.bytesize = serial.EIGHTBITS
     ser.parity = serial.PARITY_NONE
     ser.stopbits = serial.STOPBITS_ONE
