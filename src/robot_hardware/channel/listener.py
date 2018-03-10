1a572f280da3e5f01462579374f278d2f63b27b9
genou <genevieve.gilbert.6@ulaval.ca>
2018-03-10 14:31:09 -0500

    setup after further hardware testing

--- src/robot_hardware/channel/listener.py
+++ src/robot_hardware/channel/listener.py
@@ -16,11 +16,11 @@ class ChannelListener:
 def create_channel_listener():
     ser = serial.Serial()
     ser.port = "/dev/ttyUSB0"
-    ser.baudrate = 1200
+    ser.baudrate = 115200
     ser.bytesize = serial.EIGHTBITS
     ser.parity = serial.PARITY_NONE
-    ser.stopbits = serial.STOPBITS_ONE
-    ser.timeout = 0
+    ser.stopbits = 1
+    ser.timeout = 2
     ser.open()
 
     return ChannelListener(ser)
