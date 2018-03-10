9963a2b0db49f1110d532682e6b4cdf3c4cda6d5
genou <genevieve.gilbert.6@ulaval.ca>
2018-03-10 13:59:08 -0500

    add tests

--- src/robot_hardware/command_builder.py
+++ src/robot_hardware/command_builder.py
@@ -3,14 +3,14 @@ from src.robot_hardware.command import Command
 
 class CommandBuilder():
 
-    def __move(self, vx: str, vy: str, backward_forward: Command, left_right: Command) -> str:
-        return 'M{}{}{}{}'.format(vx, vy, backward_forward, left_right)
+    def move(self, vx: str, vy: str, backward_forward: Command, left_right: Command) -> str:
+        return 'M{}{}{}{}'.format(vx, vy, backward_forward.value, left_right.value)
 
     def forward(self, vx: int, vy: int, left_right: Command) -> str:
-        return self.__move(vx, vy, Command.forward.value, left_right.value)
+        return self.move(vx, vy, Command.forward, left_right)
 
     def backward(self, vx: int, vy: int, left_right: Command) -> str:
-        return self.__move(vx, vy, Command.backward.value, left_right.value)
+        return self.move(vx, vy, Command.backward, left_right)
 
     def rotate(self, angle: int, positive_negative: Command) -> str:
         return 'R{}{}'.format(angle, positive_negative.value)
