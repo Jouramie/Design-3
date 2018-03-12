0a5eed2a37b6898cc089d0e6a89acc6eda2ef42e
genou <genevieve.gilbert.6@ulaval.ca>
2018-03-12 14:44:12 -0400

    clarify speed parameters

--- src/robot_hardware/command_builder.py
+++ src/robot_hardware/command_builder.py
@@ -3,14 +3,14 @@ from src.robot_hardware.command import Command
 
 class CommandBuilder():
 
-    def move(self, vx: str, vy: str, backward_forward: Command, left_right: Command) -> str:
-        return 'M{}{}{}{}'.format(vx, vy, backward_forward.value, left_right.value)
+    def move(self, x_speed: int, y_speed: int, backward_forward: Command, left_right: Command) -> str:
+        return 'M{}{}{}{}'.format(x_speed, y_speed, backward_forward.value, left_right.value)
 
-    def forward(self, vx: int, vy: int, left_right: Command) -> str:
-        return self.move(vx, vy, Command.FORWARD, left_right)
+    def forward(self, x_speed: int, y_speed: int, left_right: Command) -> str:
+        return self.move(x_speed, y_speed, Command.FORWARD, left_right)
 
-    def backward(self, vx: int, vy: int, left_right: Command) -> str:
-        return self.move(vx, vy, Command.BACKWARD, left_right)
+    def backward(self, x_speed: int, y_speed: int, left_right: Command) -> str:
+        return self.move(x_speed, y_speed, Command.BACKWARD, left_right)
 
     def rotate(self, angle: int, positive_negative: Command) -> str:
         return 'R{}{}'.format(angle, positive_negative.value)
