df190a2b704022647639846daddf92772241dc58
jgilbert <jonathan.gilbert.9@ulaval.ca>
2018-03-20 22:10:53 -0400

    Refactor frameDrawer

--- src/vision/frameDrawer.py
+++ src/vision/frameDrawer.py
@@ -14,12 +14,17 @@ class FrameDrawer:
     def drawRobot(self, frame, robot: Robot):
         robot_corners = robot.get_corners()
 
-        camera_to_world_parameters = self.coordinateConverter.get_camera_to_world().to_parameters()
-        camera_to_world_tvec = np.array([camera_to_world_parameters[0], camera_to_world_parameters[1], camera_to_world_parameters[2]])
-        camera_to_world_rvec = np.array([camera_to_world_parameters[3], camera_to_world_parameters[4], camera_to_world_parameters[5]])
-        robot_projected_points, jac = cv2.projectPoints(robot_corners, camera_to_world_rvec, camera_to_world_tvec, self.camParam.CameraMatrix, self.camParam.Distorsion)
+        robot_projected_points = self.__projectPoints(robot_corners)
 
         cv2.line(frame, tuple(robot_projected_points[0][0]), tuple(robot_projected_points[1][0]), (204, 0, 204), 3)
         cv2.line(frame, tuple(robot_projected_points[1][0]), tuple(robot_projected_points[2][0]), (204, 0, 204), 3)
         cv2.line(frame, tuple(robot_projected_points[2][0]), tuple(robot_projected_points[3][0]), (204, 0, 204), 3)
         cv2.line(frame, tuple(robot_projected_points[3][0]), tuple(robot_projected_points[0][0]), (204, 0, 204), 3)
+
+    def __projectPoints(self, points):
+        camera_to_world_parameters = self.coordinateConverter.get_camera_to_world().to_parameters()
+        camera_to_world_tvec = np.array([camera_to_world_parameters[0], camera_to_world_parameters[1], camera_to_world_parameters[2]])
+        camera_to_world_rvec = np.array([camera_to_world_parameters[3], camera_to_world_parameters[4], camera_to_world_parameters[5]])
+        projected_points, jac = cv2.projectPoints(points, camera_to_world_rvec, camera_to_world_tvec,self.camParam.CameraMatrix, self.camParam.Distorsion)
+
+        return projected_points
