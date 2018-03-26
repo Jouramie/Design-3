10eccd548851b051bcbf57ba9d6d1a13cae1ef67
Masam121 <marcel.samson-morasse.1@ilaval.ca>
2018-03-26 15:34:10 -0400

    Fixed aruco

--- src/vision/tableManager.py
+++ src/vision/tableManager.py
@@ -1,4 +1,4 @@
-import cv2
+import cv2.aruco
 import numpy as np
 
 from src.vision.transform import Transform
@@ -22,11 +22,11 @@ class TableManager:
 
     def create_table(self, id):
         if 1 <= id <= 6:
-            camParam = cv2.aruco.CameraParameters()
-            camParam.readFromXMLFile(self.cam_path[id-1])
+            cam_param = cv2.aruco.CameraParameters()
+            cam_param.readFromXMLFile(self.cam_path[id-1])
             world_to_camera = Transform.from_matrix(np.load(self.world_calibration_path[id-1]))
 
-            return Table(id, camParam, world_to_camera)
+            return Table(id, cam_param, world_to_camera)
         else:
             raise ValueError("Invalid table number.")
 
