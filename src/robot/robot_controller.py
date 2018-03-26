2830729e192d0b8119246c852e431df3866d1722
genou <genevieve.gilbert.6@ulaval.ca>
2018-03-26 15:35:43 -0400

    fix

--- src/robot/robot_controller.py
+++ src/robot/robot_controller.py
@@ -29,14 +29,5 @@ class RobotController(object):
         self._network.wait_infrared_ask()
         self._network.send_infrared_ask(43)
 
-        commands = {'1': b'\x41\x46', '2': b'\x42\x50', '3': b'\x44\42'}
-        go = True
-        while go:
-            command = input('enter 1: activate wheel 1, 2: activate wheel 2, 3: deactivate wheels\n')
-            if command in commands:
-                self._channel.write(commands[command])
-                print(commands[command])
-            else:
-                go = False
         time.sleep(1000)
 
