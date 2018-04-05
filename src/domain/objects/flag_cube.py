5b70bc9866527c316c88ac618979c8a0341f6d2d
Masam121 <marcel.samson-morasse.1@ilaval.ca>
2018-04-05 01:16:27 -0400

    added is_placed

--- src/domain/objects/flag_cube.py
+++ src/domain/objects/flag_cube.py
@@ -5,9 +5,13 @@ class FlagCube(object):
     def __init__(self, center: tuple, color: Color):
         self.center = center
         self.color = color
+        self.is_placed = False
 
     def get_3d_corners(self):
         return [(self.center[0] + 4, self.center[1] + 4, 0), (self.center[0] - 4, self.center[1] - 4, 0)]
+
+    def place_cube(self):
+        self.is_placed = True
 
     def __str__(self):
         return "Position: {}, Color: {}".format(self.center, self.color)
