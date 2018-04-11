c22eff51e13947b137127e83013aea5d15e1ff11
genou <gegilbert@hotmail.com>
2018-04-11 16:17:59 -0400

    log

--- src/robot/robot_controller.py
+++ src/robot/robot_controller.py
@@ -129,6 +129,7 @@ class RobotController(object):
 
     def main_loop(self) -> None:
         self._start()
+        self._logger.info('AAAAAAAAAAAAAAAAAAAAAHHHHHHHHHH')
         while True:
             time.sleep(1)
             self.execute()
