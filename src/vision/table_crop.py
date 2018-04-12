e894722766c87797c141ebb63fffbe5cdc121835
willvalin <william.valin.1@ulaval.ca>
2018-04-12 10:38:12 -0400

    TableCrop for Table 4 added

--- src/vision/table_crop.py
+++ src/vision/table_crop.py
@@ -4,8 +4,9 @@ from enum import Enum
 class TableCrop(Enum):
     TABLE1 = (0, 210, 210)
     TABLE2 = (30, 230, 230)
-    TABLE3 = (12, 210, 210)
-    TABLE4 = (12, 228, 224)
+    TABLE3 = (24, 210, 210)
+    TABLE4 = (12, 230, 230)
+    TABLE5 = (12, 228, 224)
     TABLE6 = (12, 228, 224)
 
     def __init__(self, x_crop, y_crop_top, y_crop_bot):
